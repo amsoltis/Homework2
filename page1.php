33f9574d0e141ea39d1438ce239a8265e750567b
Andrew Soltis <ams.soltis@ou.edu>
2022-09-07 13:43:32 -0500

    test

---
+++ page1.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Coming Soon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">Coming Soon</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                      <li class="nav-item">
+                          <a class="nav-link active" href="./index.html">Home</a>
+                      <li class="nav-item">
+                          <a class="nav-link" href="./cat.html">Cat</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="./jobhistory.html">Job History</a>
+                      </li>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="./author.html">Author</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="./comingsoon.html">Coming Soon</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="./rubric.html">Rubric</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+      <div class="card" aria-hidden="true">
+          <div class="card-body">
+              <h5 class="card-title placeholder-glow">
+                  <span class="placeholder col-6"></span>
+              </h5>
+              <p class="card-text placeholder-glow">
+                  <span class="placeholder col-7"></span>
+                  <span class="placeholder col-4"></span>
+                  <span class="placeholder col-4"></span>
+                  <span class="placeholder col-6"></span>
+                  <span class="placeholder col-8"></span>
+              </p>
+              <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+          </div>
+      </div>
+  </body>
+</html>
+

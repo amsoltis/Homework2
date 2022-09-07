16a424e7b68cbfa13311dd2e2c42ebb474ff5b72
Andrew Soltis <ams.soltis@ou.edu>
2022-09-07 13:49:22 -0500

    test

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<?php
-$var = 5;
-?>
-<html>
-<head>
-<title>My Page</title>
-</head>
-<body>
-<h2>Your variable was <?php $var; ?></h2>
-</body>
-</php>
+<?php require_one("header.php"); ?>
+<h1> My Website </h1>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>

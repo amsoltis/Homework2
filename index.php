cc064120c6cac402c2071be4ebe148d78cbfaec1
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-09-26 13:43:27 -0500

    Update index.php
    Hello World

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <?php myFunction(); ?>
+    <?php
+function writeMsg() {
+  echo "Hello world!";
+}
+
+writeMsg(); // call the function
+?>
 
   </body>
 </html>
